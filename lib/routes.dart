553a1dd8c8d179c018cb0fcff98dbc3afd097028
evaleries <didaarda15@gmail.com>
2020-10-28 17:45:02 +0700

    Initial commit

---
+++ lib/routes.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/widgets.dart';
+
+import 'package:jobid/screens/splash/splash_screen.dart';
+
+final Map<String, WidgetBuilder> routes = {
+  SplashScreen.routeName: (context) => SplashScreen(),
+};
---
+++ lib/screens/splash/components/body.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+import 'package:jobid/constants.dart';
+import 'package:jobid/size_config.dart';
+import 'package:jobid/screens/splash/components/splash_content.dart';
+import 'package:jobid/components/button.dart';
+
+class Body extends StatefulWidget {
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  int currentPage = 0;
+
+  List<Map<String, String>> splashs = [
+    {'text': "Selamat datang di JOB.ID", 'image': "assets/images/splash_1.png"},
+    {
+      'text': "Kami dapat membantu anda\nmencari pekerjaan dengan mudah.",
+      'image': "assets/images/splash_2.png"
+    },
+    {
+      'text': "Kami dapat menampilkan\npekerjaan yang relevan untuk anda.",
+      'image': "assets/images/splash_3.png"
+    },
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SizedBox(
+        width: double.infinity,
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              flex: 3,
+              child: PageView.builder(
+                onPageChanged: (value) {
+                  setState(() {
+                    currentPage = value;
+                  });
+                },
+                itemCount: splashs.length,
+                itemBuilder: (context, index) => SplashContent(
+                  image: splashs[index]['image'],
+                  text: splashs[index]['text'],
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: getProportionateScreenWidth(20)),
+                child: Column(
+                  children: <Widget>[
+                    Spacer(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: List.generate(
+                        splashs.length,
+                        (index) => buildDot(index: index),
+                      ),
+                    ),
+                    Spacer(),
+                    DefaultButton(
+                      text: 'Lanjut',
+                      press: () {
+                        // @TODO: Go to login screen.
+                      },
+                    ),
+                    Spacer(),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  AnimatedContainer buildDot({int index}) {
+    return AnimatedContainer(
+      duration: cAnimationDuration,
+      margin: EdgeInsets.only(right: 5),
+      height: 6,
+      width: currentPage == index ? 20 : 6,
+      decoration: BoxDecoration(
+        color: currentPage == index ? cPrimaryColor : Color(0xFFD8D8D8),
+        borderRadius: BorderRadius.circular(3),
+      ),
+    );
+  }
+}
