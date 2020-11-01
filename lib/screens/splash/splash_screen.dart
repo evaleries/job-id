519db777fe15c3477a886e659867b0f2c3e15a98
evaleries <didaarda15@gmail.com>
2020-11-01 10:47:23 +0700

    Use single quote

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -4,7 +4,8 @@ import 'package:jobid/screens/splash/components/body.dart';
 import 'package:jobid/size_config.dart';
 
 class SplashScreen extends StatelessWidget {
-  static String routeName = "/splash";
+  static String routeName = '/splash';
+
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
