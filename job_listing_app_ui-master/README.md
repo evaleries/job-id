00067917d0c4d55a173dd55adce2764cbd75ce6a
evaleries <48923153+evaleries@users.noreply.github.com>
2021-01-15 23:03:11 +0700

    Refactor Home Screen

--- job_listing_app_ui-master/README.md
+++
@@ -1,4 +0,0 @@
-# joblisting_app_ui
-
-This repository is based on converting this mockup into a flutter app.
-
--- job_listing_app_ui-master/android/app/src/main/kotlin/com/example/job_listing_app_ui/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.job_listing_app_ui
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
